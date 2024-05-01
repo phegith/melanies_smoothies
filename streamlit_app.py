86072b6f5cc329d9c20079169e06ec0257e6cae7
phegith <168552858+phegith@users.noreply.github.com>
2024-05-01 10:50:42 +0200

    Update streamlit_app.py
    show nut info per fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,8 @@ if ingredients_list:
         INGREDIENTS_STRING = ''
         for fruit_chosen in ingredients_list:
             INGREDIENTS_STRING += fruit_chosen + ' '
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
         #st.write(INGREDIENTS_STRING) 
 
